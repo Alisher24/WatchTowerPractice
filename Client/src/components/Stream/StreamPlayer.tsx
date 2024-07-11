b5e85f7c7ea33014e0bdd2931863697addbfa8a5
artiartem <a_shitikov8@mail.ru>
2024-07-11 13:40:05 +0700

    Jsmpeg setted

--- Client/src/components/Stream/StreamPlayer.tsx
+++ Client/src/components/Stream/StreamPlayer.tsx
@@ -1,37 +1,20 @@
 import React, {useEffect, useRef, useState} from 'react';
-import { FFmpeg } from '@ffmpeg/ffmpeg';
-
+import jsmpeg from "jsmpeg"
 interface StreamPlayerProps {
   wsUrl: string;
 }
 
-const ffmpeg = new FFmpeg();
-// FIXME: error on loading ffmpeg
-// await ffmpeg.load();
 
 const StreamPlayer: React.FC<StreamPlayerProps> = ({ wsUrl }) => {
-  const [loaded, setLoaded] = useState(false);
-  const ffmpegRef = useRef(new FFmpeg());
-  const videoRef = useRef<HTMLVideoElement | null>(null);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const messageRef = useRef<HTMLParagraphElement | null>(null)
     const startStream = async () => {
-      const ffmpeg = ffmpegRef.current;
       const ws = new WebSocket(wsUrl);
+      new jsmpeg(ws, {canvas: canvasRef.current})
       ws.binaryType = 'arraybuffer';
       ws.onmessage = async (event) => {
         const videoData = new Uint8Array(event.data);
         console.log("web buffer:", videoData);
-        // await ffmpeg.writeFile('input.ts', videoData);
-        //
-        // await ffmpeg.exec(['-i', 'input.ts', 'output.mp4']);
-        //
-        // const fileData = await ffmpeg.readFile('output.mp4');
-        // const data = new Uint8Array(fileData as ArrayBuffer);
-        // if (videoRef.current) {
-        //   videoRef.current.src = URL.createObjectURL(
-        //     new Blob([data.buffer], { type: 'video/mp4' })
-        //   )
-        // }
       };
 
       ws.onclose = () => {
@@ -45,7 +28,7 @@ const StreamPlayer: React.FC<StreamPlayerProps> = ({ wsUrl }) => {
 
 
   return <>
-    <video ref={videoRef} className="streamPlayer" controls/>
+    <canvas ref={canvasRef} className="streamPlayer" controls/>
     <p ref={messageRef}></p>
     <button onClick={startStream}>Load ffmpeg-core</button>
   </>;
--- Client/src/pages/HomePage/HomePage.tsx
+++ Client/src/pages/HomePage/HomePage.tsx
@@ -15,7 +15,7 @@ interface Camera {
 }
 
 const HomePage: React.FC = () => {
-  const isAuthenticated = Boolean(localStorage.getItem('token')) || true;
+  const isAuthenticated = Boolean(localStorage.getItem('token')) || true; // TODO: delete "|| true"
   const [activeStreams, setActiveStreams] = useState<Camera[]>([]);
 
   const handleStartStream = (camera: Camera) => {
