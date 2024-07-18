2b55756cd4965a6000956c6b082ad255b2fb8d58
artiartem8 <a_shitikov8@mail.ru>
2024-07-18 11:37:56 +0700

    Destroy player

--- Client/src/components/Stream/StreamPlayer.tsx
+++ Client/src/components/Stream/StreamPlayer.tsx
@@ -1,30 +1,24 @@
 import React, {useEffect, useRef, useState} from 'react';
+import JSMpeg from 'jsmpeg-player';
+
 interface StreamPlayerProps {
-  wsUrl: string;
+    wsUrl: string;
 }
 
 
-const StreamPlayer: React.FC<StreamPlayerProps> = ({ wsUrl }) => {
-  const canvasRef = useRef<HTMLCanvasElement | null>(null);
-
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "/js/jsmpeg.min.js";
-    script.onload = () => {
-      // @ts-ignore
-      const player = new JSMpeg.Player(wsUrl, { canvas: canvasRef.current});
-
-      return () => {
-        player.destroy();
-      };
-    };
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, [wsUrl]);
-
-  return <canvas ref={canvasRef} className="streamPlayer" />;
+const StreamPlayer: React.FC<StreamPlayerProps> = ({wsUrl}) => {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+    const startStream = () => new JSMpeg.Player(wsUrl, {canvas: canvasRef.current, autoplay: true});
+    useEffect(() => {
+        const stream = startStream();
+        return () => {
+            stream.destroy();
+        }
+    }, [])
+    return <>
+        <canvas ref={canvasRef} className="streamPlayer" controls/>
+        {/*<button onClick={startStream}>Load ffmpeg-core</button>*/}
+    </>;
 };
+
 export default StreamPlayer;
--- Client/src/pages/HomePage/HomePage.tsx
+++ Client/src/pages/HomePage/HomePage.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import CameraList from '../../components/Camera/CameraList';
 import CameraRegister from '../../components/Camera/CameraRegister';
 import VideoGrid from '../../components/Camera/VideoGrid';
-import StreamPlayer from '../../components/Stream/StreamPlayer';
+// import StreamPlayer from '../../components/Stream/StreamPlayer';
 import apiService from '../../services/api/auth.service';
 import './HomePage.css';
 import {Link} from "react-router-dom";
@@ -24,19 +24,18 @@ const HomePage: React.FC = () => {
     if (activeStreams.find(stream => stream.id === camera.id)) return; // do nothing if stream is active
     setActiveStreams(prevStreams => [...prevStreams, camera]); // TODO: delete line
     apiService.startStream(camera.ip, camera.name, camera.password)
-      .then((url) => {
-        console.log("WSURL", url); // debug
-        const wsUrl = `ws://localhost:5003/stream/${url}`;
-        camera.wsUrl = wsUrl;
-        setActiveStreams(prevStreams => [...prevStreams, camera]);
-      })
-      .catch(error => {
-        console.error('Ошибка подключения к камере:', error);
-      });
+        .then((url) => {
+          console.log("WSURL", url); // debug
+          const wsUrl = `ws://localhost:5003/stream/${url}`;
+          camera.wsUrl = wsUrl;
+          setActiveStreams(prevStreams => [...prevStreams, camera]);
+        })
+        .catch(error => {
+          console.error('Ошибка подключения к камере:', error);
+        });
   };
   const handleStopStream = (camera: Camera) => {
-    setActiveStreams(prevStreams => prevStreams.filter(stream => stream.id !== camera.id)); // TODO: delete line
-
+    setActiveStreams(prevStreams => prevStreams.filter(stream => stream.id !== camera.id));
   };
 
 
@@ -46,43 +45,43 @@ const HomePage: React.FC = () => {
     // window.location.reload();
   };
   return (
-    <div className="homePage">
-      {isAuthenticated ? (
-        <>
-          <div className="topRight">
-            <button onClick={exitUser}>Exit</button>
-          </div>
-          <div className="sidebar">
-            <CameraList onStartStream={handleStartStream} onStopStream={handleStopStream}/>
-            <CameraRegister/>
-          </div>
-          <div className="content">
-            <VideoGrid activeStreams={activeStreams} handleStopStream={handleStopStream}/>
-            {/*<div className="video-grid">*/}
-            {/*{activeStreams.map((camera, index) => (*/}
-            {/*  <div key={index} className="streamContainer">*/}
-            {/*    <StreamPlayer wsUrl={`ws://localhost:5003/${camera.id}`}/>*/}
-            {/*    /!*<StreamPlayer wsUrl={'ws://localhost:5003/stream/192.168.50.165/ws'}/>*!/*/}
-            {/*    <button type="button" className="btn-close" onClick={() => handleStopStream(camera)}>*/}
-            {/*      /!*<span className="icon-cross"></span>*!/*/}
-            {/*      <div className="cross"><span></span></div>*/}
-            {/*      /!*<span className="visually-hidden">Close</span>*!/*/}
-            {/*    </button>*/}
-            {/*    <center>{camera.title}</center>*/}
-            {/*  </div>*/}
-            {/*))*/}
-            {/*}*/}
-            {/*</div>*/}
-            {activeStreams.length == 0 && <img style={{alignSelf: 'center'}} src="https://github.com/fluidicon.png" alt="example image"/>}
-          </div>
-        </>
-      ) : (
-        <div style={{margin: '40vh auto'}}>
-          <p>Please login to view and register cameras.</p>
-          <Link to="/login">Login</Link>
-        </div>
-      )}
-    </div>
+      <div className="homePage">
+        {isAuthenticated ? (
+            <>
+              <div className="topRight">
+                <button onClick={exitUser}>Exit</button>
+              </div>
+              <div className="sidebar">
+                <CameraList onStartStream={handleStartStream} onStopStream={handleStopStream}/>
+                <CameraRegister/>
+              </div>
+              <div className="content">
+                <VideoGrid activeStreams={activeStreams} handleStopStream={handleStopStream}/>
+                {/*<div className="video-grid">*/}
+                {/*{activeStreams.map((camera, index) => (*/}
+                {/*  <div key={index} className="streamContainer">*/}
+                {/*    <StreamPlayer wsUrl={`ws://localhost:5003/${camera.id}`}/>*/}
+                {/*    /!*<StreamPlayer wsUrl={'ws://localhost:5003/stream/192.168.50.165/ws'}/>*!/*/}
+                {/*    <button type="button" className="btn-close" onClick={() => handleStopStream(camera)}>*/}
+                {/*      /!*<span className="icon-cross"></span>*!/*/}
+                {/*      <div className="cross"><span></span></div>*/}
+                {/*      /!*<span className="visually-hidden">Close</span>*!/*/}
+                {/*    </button>*/}
+                {/*    <center>{camera.title}</center>*/}
+                {/*  </div>*/}
+                {/*))*/}
+                {/*}*/}
+                {/*</div>*/}
+                {activeStreams.length == 0 && <img style={{alignSelf: 'center'}} src="https://github.com/fluidicon.png" alt="example image"/>}
+              </div>
+            </>
+        ) : (
+            <div style={{margin: '40vh auto'}}>
+              <p>Please login to view and register cameras.</p>
+              <Link to="/login">Login</Link>
+            </div>
+        )}
+      </div>
   );
 };
 
